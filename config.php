ab51b8805ae672e6e45c0ddd2a749d3665956a0f
Arthur Falcão <arthurfalcao77@gmail.com>
2017-10-22 04:04:03 -0300

    Adicionando Cadastro e conectando com o banco/servidor

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php 
+
+$con = @mysqli_connect("localhost", "root", "") or die("Não foi possível conectar com o servidor!");
+
+
+
+/*define('DB_NAME', 'db_crud');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+
+if (!defined('ABSPATH')) {
+	define('ABSPATH', dirname(__FILE__) . '/');
+}
+if (!defined('BASEURL')) {
+	define('BASEURL', '/user-crud/');
+}
+if (!defined('DBAPI')) {
+	define('DBAPI', ABSPATH . 'inc/database.php');
+}*/
+ ?>
---
+++ inc/database.php
@@ -0,0 +1,9 @@
+<?php 
+
+	mysqli_report(MYSQLI_REPORT_STRICT);
+
+	function open_database(){
+		
+	}
+
+ ?>
