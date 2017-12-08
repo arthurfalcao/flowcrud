99da85e9f54b4f1407d92db70447963771885f0d
Arthur Falcão <arthurfalcao77@gmail.com>
2017-12-08 02:01:23 -0300

    config

--- config.php
+++ config.php
@@ -1,16 +1,15 @@
-<?php 
+<?php
 
 $host = "localhost";
-$db_name = "db_crud";
-$username = "root";
-$passaword = "";
+$db_name = "id3384319_db_crud";
+$username = "id3384319_arthurfalcao";
+$password = "flow123";
 
 try {
-	$conexao = new PDO("mysql:host={$host}; dbname={$db_name}", $username, $passaword);
+	$conexao = new PDO("mysql:host={$host}; dbname={$db_name}", $username, $password);
 } catch (PDOException $erro) {
 	echo "Erro na conexão: " . $erro->getMessage();
 }
 
 require_once 'funcoes.php';
-
- ?>
+?>
