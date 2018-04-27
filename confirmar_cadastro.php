e0e00423cb82af62273a44c11f1c3e80b04a07d8
Arthur Falcão <arthurfalcao77@gmail.com>
2018-04-27 17:34:09 -0300

    using function insertUser

--- confirmar_cadastro.php
+++ confirmar_cadastro.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'config.php';
+include 'grvusuario.php';
 
 $nome = $_POST['nome'];
 $email = $_POST['email'];
@@ -7,18 +8,10 @@ $senha = md5($_POST['senha']);
 $cidade = $_POST['cidade'];
 $uf = $_POST['uf'];
 
-$SQL = "INSERT INTO T_USUARIO (id, nome, email, senha, cidade, uf) VALUES (DEFAULT, ?, ?, ?, ?, ?)";
-$stmt = $conexao->prepare($SQL);
-$stmt->bindParam(1, $nome);
-$stmt->bindParam(2, $email);
-$stmt->bindParam(3, $senha);
-$stmt->bindParam(4, $cidade);
-$stmt->bindParam(5, $uf);
-
-if($stmt->execute()){
+if(insertUser($conexao, $nome, $email, $senha, $cidade, $uf)) {
     echo "<script>alert('Usuário cadastrado com sucesso');</script>";
-    echo "<script language=\"javascript\">window.location=\"login.php\";</script>"; 
-}else{
+    echo "<script language=\"javascript\">window.location=\"login.php\";</script>";
+} else {
     echo "<script>alert('Email já cadastrado'); history.back();</script>";
 }
  ?>
