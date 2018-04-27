1e8153697ae8cac0cd358c958ff1e78cf3a61243
Arthur Falcão <arthurfalcao77@gmail.com>
2018-04-27 17:33:20 -0300

    function insertUser and removeUser

--- grvusuario.php
+++ grvusuario.php
@@ -12,3 +12,21 @@ function listaUsuarios($conexao) {
   }
   return $usuarios;
 }
+
+function insertUser($conexao, $nome, $email, $senha, $cidade, $uf) {
+  $query = "INSERT INTO T_USUARIO (id, nome, email, senha, cidade, uf) VALUES (DEFAULT, ?, ?, ?, ?, ?)";
+  $stmt = $conexao->prepare($query);
+  $stmt->bindParam(1, $nome);
+  $stmt->bindParam(2, $email);
+  $stmt->bindParam(3, $senha);
+  $stmt->bindParam(4, $cidade);
+  $stmt->bindParam(5, $uf);
+  return $stmt->execute();
+}
+
+function removeUsuario($conexao, $id) {
+  $query = "DELETE FROM T_USUARIO WHERE id = ?";
+  $stmt = $conexao->prepare($query);
+  $stmt->bindParam(1, $id);
+  return $stmt->execute();
+}
